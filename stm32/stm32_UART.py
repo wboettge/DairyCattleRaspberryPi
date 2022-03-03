8cecc2ebf29ddfe8de276c21c2aa399ded145c57
Walt Boettge <wboettge@wisc.edu>
2022-03-03 11:18:31 -0600

    remove extra newlines from UART read

--- stm32/stm32_UART.py
+++ stm32/stm32_UART.py
@@ -6,7 +6,7 @@ BAUD_RATE = 115200
 ser = serial.Serial(port=UART_PORT, baudrate=115200)
 try:
     while(True):
-        print(str(ser.readline(), 'utf8'))
+        print(str(ser.readline(), 'utf8'), end='')
 
 except KeyboardInterrupt:
     print('Closing')
