d651f7258508d5dc793db476bd672d56fb94740c
Walt Boettge <wboettge@wisc.edu>
2022-03-03 11:02:42 -0600

    fix call to readline()

--- stm32/stm32_UART.py
+++ stm32/stm32_UART.py
@@ -4,13 +4,13 @@ UART_PORT = '/dev/ttyACM0'
 BAUD_RATE = 115200
 
 ser = serial.Serial(port=UART_PORT, baudrate=115200)
-
+print(ser.name)
 try:
     while(True):
-        print(ser.readLine())
+        print(ser.readline())
 
 except KeyboardInterrupt:
     print('Closing')
-    
+
 finally:
     ser.close()
