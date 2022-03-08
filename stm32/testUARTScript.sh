b33d7ac881ed9de2a5fa0e699bdea17ea94ec7d4
Lia <sudjana@wisc.edu>
2022-03-08 12:07:12 -0600

    add testUARTScript.sh

---
+++ stm32/testUARTScript.sh
@@ -0,0 +1,1 @@
+python3 ~/DairyCattleRaspberryPi/TempSensorTesting/publishUARTData.py --root-ca /etc/iot-certificates/root.ca.pem --cert /etc/iot-certificates/75969a21f7-certificate.pem.crt --key /etc/iot-certificates/75969a21f7-private.pem.key --endpoint a2q9slo0dyelxs-ats.iot.us-east-1.amazonaws.com --count 0 --timeout 120 & 
