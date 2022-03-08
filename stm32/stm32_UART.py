5d2f532173c5d308a363957b4b6cae7473df85b0
Walt Boettge <wboettge@wisc.edu>
2022-03-08 10:51:39 -0600

    read messages from stm32 module over UART

--- stm32/stm32_UART.py
+++ stm32/stm32_UART.py
@@ -3,7 +3,7 @@ import serial
 UART_PORT = '/dev/ttyACM0'
 BAUD_RATE = 115200
 
-ser = serial.Serial(port=UART_PORT, baudrate=115200)
+ser = serial.Serial(port=UART_PORT, baudrate=9600)
 try:
     while(True):
         print(str(ser.readline(), 'utf8'), end='')
